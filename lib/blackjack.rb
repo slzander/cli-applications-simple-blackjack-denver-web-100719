9d951c7941129201197ff9f4f936cd4222b2d84a
Stacey Zander <staceyzander@gmail.com>
2019-10-01 19:54:00 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -54,10 +54,12 @@ end
 
 def runner
   welcome
-  card_total = display_card_total(hit?(initial_round))
-#  display_card_total(hit?)
+  card_total = initial_round
+  hit?(card_total)
+  display_card_total(card_total)
   until card_total > 21 do
-    display_card_total(hit?(card_total))
+    card_total = hit?(card_total)
+    display_card_total(card_total)
   end
   end_game
 end
